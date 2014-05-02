77e10d4af686d20dd954cb3c0d799460e7a44580
Shiv Pande <shivpande@Shivs-MacBook-Pro.local>
2014-05-01 18:39:21 -0700

    second commit

---
+++ SHIV-IOS2014/SHIV-IOS2014/SKPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SKPAppDelegate.h
+//  SHIV-IOS2014
+//
+//  Created by Shiv Pande on 5/1/14.
+//  Copyright (c) 2014 Shiv Pande. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SHIV-IOS2014/SHIV-IOS2014/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SHIV-IOS2014
+//
+//  Created by Shiv Pande on 5/1/14.
+//  Copyright (c) 2014 Shiv Pande. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SKPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SKPAppDelegate class]));
+    }
+}
