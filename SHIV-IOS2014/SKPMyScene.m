b4ca492732db09fc81d7982745d89aa39b0ce91b
Joshua Howland <howland_josh@yahoo.com>
2014-05-17 23:35:06 -0700

    Fixed tree overlaying perspective issue (made their zposition value equal to the width, always places bigest trees in front.  Fixes perspective illusion.  Also tweaked explosion powerup

--- SHIV-IOS2014/SKPMyScene.m
+++ SHIV-IOS2014/SKPMyScene.m
@@ -142,27 +142,28 @@
     for (UITouch *touch in touches) {
         CGPoint location = [touch locationInNode:self];
         //_player.position = CGPointMake(location.x, _player.position.y);
-        int dX = -6.6*(_lastTouch.x-location.x);
+        int dX = -4.6*(_lastTouch.x-location.x);
         int newX = _player.position.x + dX;
-        int dY = -_lastTouch.y + location.y;
-        if(dY>self.size.height/20){ //should try powerup..
+        int dY = abs(-_lastTouch.y + location.y);
+        if(dY>self.size.height/40){ //should try powerup..
             [self blastPowerUp];
-        }
-        if(_player.position.x<self.size.width/10){
-            if(dX>0){
-                _player.position = CGPointMake(newX, _player.position.y);
-            }
-        }else if(_player.position.x>self.size.width*.9){
-            if(dX<0){
-                _player.position = CGPointMake(newX, _player.position.y);
-            }
         }else{
-            if(newX>self.size.width*.9){
-                //do nothing
-            }else if(newX<self.size.width*.1){
-                //do nothing
+            if(_player.position.x<self.size.width/10){
+                if(dX>0){
+                    _player.position = CGPointMake(newX, _player.position.y);
+                }
+            }else if(_player.position.x>self.size.width*.9){
+                if(dX<0){
+                    _player.position = CGPointMake(newX, _player.position.y);
+                }
             }else{
-                _player.position = CGPointMake(newX, _player.position.y);
+                if(newX>self.size.width*.9){
+                    //do nothing
+                }else if(newX<self.size.width*.1){
+                    //do nothing
+                }else{
+                    _player.position = CGPointMake(newX, _player.position.y);
+                }
             }
         }
         //if((_player.position.x>0) && (_player.position.x<self.size.width)){
@@ -202,7 +203,7 @@
 
 
 -(void)blastPowerUp{
-    for(int i=0;i<_blocks.count;i++){
+    for(int i=(int)_blocks.count-1;i>=0;i--){
         Block *tempBlock = _blocks[i];
         SKSpriteNode *nTemp = tempBlock.blockSprite;
         
@@ -271,7 +272,7 @@
         if((_time%5==0) && (arc4random()%4==0)){
             Tree *treeTemp = [[Tree alloc] initWithPosition:_treeSpawn andScreenSize:self.size];
             [_trees addObject:treeTemp];
-            treeTemp.zPosition = (int)100-abs(treeTemp.spawn.x-self.size.width/2);
+           // treeTemp.zPosition = (int)100-abs(treeTemp.spawn.x-self.size.width/2);
             [self addChild:treeTemp.treeSprite];
         }
         
